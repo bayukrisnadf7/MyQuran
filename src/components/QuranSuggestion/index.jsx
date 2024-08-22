bc0c4be79689daad1a8f87bc1b335ac6ed4c0753
bayukrisna <bayukrisna14658@gmail.com>
2024-08-23 01:10:24 +0700

    detail surah done

--- src/components/QuranSuggestion/index.jsx
+++ src/components/QuranSuggestion/index.jsx
@@ -4,8 +4,8 @@ const QuranSuggestion = ({ api }) => {
   const { data } = api;
   return (
     <div className="bg-color-quaternary w-max p-2 rounded-lg">
-      <Link href={`/quran/${data.nomor}`}>
-        <p className="text-color-tertiary">{data.namaLatin}</p>
+      <Link href={`/quran/${data.number}`}>
+        <p className="text-color-tertiary">{data.name?.transliteration.id}</p>
         
       </Link>
     </div>
